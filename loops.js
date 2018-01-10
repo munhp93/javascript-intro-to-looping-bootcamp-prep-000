beb83b3b2eb188d52e01fcbbe10e1139a03889df
Hoang Phuc Nguyen <munhp93@gmail.com>
2018-01-10 00:41:15 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,21 +8,3 @@ function forLoop(array){
   }
   return array;
 }
-
-function whileLoop(n){
-  while ( n > 0 ){
-    console.log(--n);
-  }
-  return "done"
-}
-
-function doWhileLoop(array){
-  function maybeTrue(){
-    return Math.random()>= 0.5;
-  }
-
-do {
-  array = array.slice(1)
-}
-while(array.length>0 && maybeTrue())
-return array
